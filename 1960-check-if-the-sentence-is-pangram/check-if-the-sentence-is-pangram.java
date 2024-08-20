09dd490f97ed4663da32d3f6d4b44d3949cd8266
Kanishkar T <166539631+Kanishkarthangaraj@users.noreply.github.com>
2024-08-20 15:58:41 +0530

    Time: 4 ms (37.34%) | Memory: 41.4 MB (79.33%) - LeetSync

---
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+        HashSet<Character> map=new HashSet<>();
+        for(int i=0;i<sentence.length();i++){
+            map.add(sentence.charAt(i));
+        }
+        if(map.size()==26){
+           return true;
+        }
+        return false;
+
+    }
+}
